0f0e9efc02e1544890b7f7b6306ed01c45930d1d
mascal <116852972+mascaldotfr@users.noreply.github.com>
2023-06-09 00:44:13 +0200

    UI/menu: make main links bold

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,10 @@
 let menu = `
 	<div id="menu">
-		<a href="index.html">Trainer</a>
+		<b><a href="index.html">Trainer</a></b>
 		&nbsp;|&nbsp;
-		<a href="bosses.html">Bosses countdown</a>
+		<b><a href="bosses.html">Bosses</a></b>
 		&nbsp;|&nbsp;
-		<a href="bz.html">BZ status</a>
+		<b><a href="bz.html">BZ status</a></b>
 		&nbsp;|&nbsp;
 		<a href="https://github.com/mascaldotfr/CoRT" target="_blank">Source Code</a>
 		&nbsp;|&nbsp;
