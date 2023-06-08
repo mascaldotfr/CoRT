3805e4be1d2be698f414956c34cc66623d8ce72f
mascal <116852972+mascaldotfr@users.noreply.github.com>
2023-06-08 12:39:04 +0200

    UI: use links for reporting bugs instead of cluttering the UI

--- js/menu.js
+++ js/menu.js
@@ -5,6 +5,10 @@ let menu = `
 		<a href="bosses.html">Bosses countdown</a>
 		&nbsp;|&nbsp;
 		<a href="bz.html">BZ status</a>
+		&nbsp;|&nbsp;
+		<a href="https://github.com/mascaldotfr/CoRT" target="_blank">Source Code</a>
+		&nbsp;|&nbsp;
+		<a href="https://github.com/mascaldotfr/CoRT/wiki/Bug-reports" target="_blank">Bug Reports</a>
 	</div>
 	`;
 $(document).ready(function() {
