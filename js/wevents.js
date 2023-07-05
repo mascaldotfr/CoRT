f4e513785d559a763fa383958aa456d584e99a31
mascal <116852972+mascaldotfr@users.noreply.github.com>
2023-07-05 21:15:05 +0200

    WEVENTS: big refactoring
    1. Add a date selector and display events day by day. For convenience
       sliders has been added
    2. Remove infinite scrolling as a result
    3. Refactor the filters to avoid slow copy of events

--- js/wevents.js
+++ js/wevents.js
@@ -15,77 +15,52 @@
  * along with CoRT.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-let __wevents__seek = 0;
-let __wevents__step = 100;
 let __wevents__filter = "none";
+let __wevents__dates = [];
 let data = null;
 
-async function display_events(clear=false) {
-	if (data === null)
-		await get_data();
+function day_filter(events, dayts) {
+	let base_ts = new Date(dayts * 1000);
+	base_ts.setHours(0, 0, 0);
+	let min_ts = Math.floor(base_ts.getTime() / 1000);
+	base_ts.setHours(23, 59, 59);
+	let max_ts = Math.floor(base_ts.getTime() / 1000);
+	return events.filter(i => i["date"] >= min_ts && i["date"] <= max_ts);
+}
+
+function display_events() {
+	let filtered = [];
 	if (__wevents__filter == "none") {
-		let slice = data.slice(__wevents__seek, __wevents__seek + __wevents__step);
-		var html = humanise_events(slice, false);
-		if (__wevents__seek + __wevents__step > data.length)
-			__wevents__seek = data.length - __wevents__step;
-		__wevents__seek += __wevents__step;
+		filtered = data;
 	}
-	else {
-		// Fetch everything in one swoop if they are filters
-		var slice = data.slice(__wevents__seek, data.length);
-		__wevents__seek = data.length;
-	}
-	if (__wevents__filter == "noforts") {
-		let filtered = [];
-		slice.filter(function (item) {
-			if (item["name"].startsWith("Great Wall of") || item["type"] != "fort")
-				filtered.push(item);
-		});
-		var html = humanise_events(filtered, false);
+	else if (__wevents__filter == "noforts") {
+		filtered = data.filter(i => i["name"].startsWith("Great Wall of") ||
+					    i["type"] != "fort");
 	}
 	else if (__wevents__filter == "invas") {
-		let filtered = [];
-		slice.filter(function (item) {
-			if (item["name"].startsWith("Great Wall of") && 
-			    item["owner"] != item["location"])
-				filtered.push(item);
-		});
-		var html = humanise_events(filtered, false);
+		filtered = data.filter(i => i["name"].startsWith("Great Wall of") &&
+					    i["owner"] != i["location"]);
 	}
 	else if (__wevents__filter == "gems") {
-		let filtered = [];
-		slice.filter(function (item) {
-			if (item["type"] == "gem")
-				filtered.push(item);
-		});
-		var html = humanise_events(filtered, false);
+		filtered = data.filter(i => i["type"] == "gem");
 	}
 	else if (__wevents__filter == "wishes") {
-		let filtered = [];
-		slice.filter(function (item) {
-			if (item["type"] == "wish")
-				filtered.push(item);
-		});
-		var html = humanise_events(filtered, false);
+		filtered = data.filter(i => i["type"] == "wish");
 	}
-	if (clear === true) {
-		$("#we-events").empty();
-		$("#we-events").html(html);
-	}
-	else {
-		$("#we-events").append(html);
-	}
+	let ondate = $("#we-date").val();
+	if (ondate != "none")
+		filtered = day_filter(filtered, ondate);
+	let html = humanise_events(filtered, false);
+	if (html.length == 0)
+		html = _("No matching event found!");
+	$("#we-events").empty();
+	$("#we-events").html(html);
 }
 
 async function get_data() {
 	try {
-			data = await $().getJSON("https://hail.thebus.top/cortdata/warstatus/stats/events.json");
-			$("#we-info-error").empty();
-			// Keep static values here
-			let generated = new Date(data.shift()["generated"] * 1000);
-			let datetime = generated.toLocaleTimeString(undefined,
-				{hour: "2-digit", minute: "2-digit"});
-			$("#we-info-updated").text(datetime);
+		data = await $().getJSON("https://hail.thebus.top/cortdata/warstatus/stats/events.json");
+		$("#we-info-error").empty();
 	}
 	catch (error) {
 		$("#we-info-error").html(`<b>Failed to get the events:</b> <code>${error}</code>`);
@@ -93,12 +68,57 @@ async function get_data() {
 	}
 }
 
-$(document).ready(function() {
-	$("#we-filter").on("change", function () {
-			__wevents__filter = $("#we-filter").val();
-			__wevents__seek = 0;
-			display_events(true);
-	});
+function date_change(direction) {
+	let selector = document.getElementById("we-date");
+	let index = selector.selectedIndex;
+	index += direction == "next" ? 1 : -1;
+	if (index < 0) {
+		$("#we-prev-day").hide();
+	}
+	else if (index >= __wevents__dates.length) {
+		$("#we-next-day").hide();
+	}
+	else { // valid value
+		if (index != 0)
+			$("#we-prev-day").show();
+		else
+			$("#we-prev-day").hide();
+		if (index < __wevents__dates.length - 1)
+			$("#we-next-day").show();
+		else
+			$("#we-next-day").hide();
+		selector.options[index%selector.options.length].selected = true;
+		$("#we-date").trigger("change");
+	}
+}
+
+$(document).ready(async function() {
+	await get_data();
+	let generated = new Date(data.shift()["generated"] * 1000);
+	generated = generated.toLocaleTimeString(undefined,
+		{hour: "2-digit", minute: "2-digit"});
+	$("#we-info-updated").text(generated);
+
+	let now = new Date();
+	now.setHours(0, 0, 0);
+	let dtformat = {day: "2-digit", month: "2-digit"};
+	let first_event = new Date(data.slice(-1)[0]["date"] * 1000);
+	let generate_days = (now.getTime() - first_event.getTime()) / (1000 * 3600 * 24);
+	let day_select_html = "";
+	generate_days = Math.ceil(generate_days) + 1;
+	for (let i = 0; i < generate_days; i++) {
+		let previous = new Date();
+		previous.setDate(now.getDate() - i);
+		previous.setHours(0, 0, 0);
+		let previous_ts = previous.getTime() / 1000;
+		__wevents__dates.push(previous_ts);
+		day_select_html += `
+			<option value="${previous_ts}">
+				${previous.toLocaleDateString(undefined, dtformat)}
+			</option>`;
+	}
+	$("#we-date").append(day_select_html);
+
 	let urlsearch = new URLSearchParams(window.location.search);
 	let filter = urlsearch.get("f");
 	if (!filter) {
@@ -109,12 +129,10 @@ $(document).ready(function() {
 		$("#we-filter").val(filter);
 		$("#we-filter").trigger("change");
 	}
-});
 
-
-
-$(window).on("scroll", () => {
-	if ( window.scrollY + window.innerHeight >= document.body.offsetHeight - 500) {
+	$("#we-filter").on("change", function () {
+		__wevents__filter = $("#we-filter").val();
 		display_events();
-	}
+	});
+	$("#we-date").on("change", display_events)
 });
