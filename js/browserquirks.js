9a0eb19ab89fed296ebadf60f529555a4072c2b5
mascal <116852972+mascaldotfr@users.noreply.github.com>
2023-07-08 01:18:05 +0200

    GLOBAL: add an unsupported browser handler
    Visiting the site with a browser not supporting async/await will display
    a message prompting the user to upgrade their browsers.

--- js/browserquirks.js
+++ js/browserquirks.js
@@ -28,3 +28,23 @@ function loademojis() {
 	head.appendChild(style);
 };
 loademojis();
+
+// Website requires working async/await and by the way detects non ES8 browser
+function async_or_die() {
+	try {
+		eval("async () => {}");
+	}
+	catch (_unused) {
+		console.log("async() failed");
+		document.body.innerHTML = "" +
+			"<h1>Your browser is too outdated for using CoRT. " +
+			"Please <a href='http://browser-update.org/update-browser.html'>upgrade your browser</a>.</h1>";
+		document.body.appendChild(e);
+	}
+}
+try {
+	document.addEventListener("DOMContentLoaded", async_or_die);
+}
+catch (_unused) {
+	window.attachEvent("onload", async_or_die);
+}
