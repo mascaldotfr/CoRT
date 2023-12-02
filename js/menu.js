57339c7ff4f39f5dacc2c1f69a6a86f897b76787
mascal <116852972+mascaldotfr@users.noreply.github.com>
2023-12-02 17:16:44 +0100

    API: add preconnect to my server early
    It will allow to make the pages using data from my server faster on the
    first visit for non EU visitors.

--- js/menu.js
+++ js/menu.js
@@ -28,7 +28,7 @@ let footer = `
 		<p><i>CoRT is a free and open source website, feel free to check out its
 		<a href="https://github.com/mascaldotfr/CoRT" target="_blank">source code</a>, and report
 		<a href="https://github.com/mascaldotfr/CoRT/wiki/Bug-reports" target="_blank">bugs</a>.
-		<!--VERSION-->Version: 20231202.153822
+		<!--VERSION-->Version: 20231202.161644
 		</i></p>
 `;
 
