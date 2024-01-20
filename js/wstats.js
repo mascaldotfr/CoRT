b13829d1643af061f21c398f445592420c2721de
mascal <116852972+mascaldotfr@users.noreply.github.com>
2024-01-20 21:22:34 +0100

    WSTATS: expand "to the top" links to titles themselves.

--- js/wstats.js
+++ js/wstats.js
@@ -7,7 +7,7 @@ import {timestamp_ago, timestamp_now} from "./wztools/time.js";
 import {$onlinemanager} from "./onlinemanager.js";
 import {__chartist_responsive} from "./chartist-1.3.0.min.js";
 
-let to_the_top = `&nbsp;<a href="#ws-index-title"><svg style="height:1.5em; margin-bottom:0px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M0 0h512v512H0z" fill-opacity="0"></path><path d="M256 47L139.4 202.467l93.6-40.115V359h46V162.352l93.6 40.115L256 47zM144 256L32 480h448L368 256h-71v121h-82V256h-71z" fill="#9ccdf9" fill-opacity="1"></path></svg></a>`;
+let to_the_top = `&nbsp;<svg style="height:1.5em; margin-bottom:0px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M0 0h512v512H0z" fill-opacity="0"></path><path d="M256 47L139.4 202.467l93.6-40.115V359h46V162.352l93.6 40.115L256 47zM144 256L32 480h448L368 256h-71v121h-82V256h-71z" fill="#9ccdf9" fill-opacity="1"></path></svg>`;
 
 // sync with statistics.json
 let report_days = [7, 30, 90];
@@ -223,7 +223,7 @@ $(document).ready(function() {
 	$("#ws-index-title").text(_("Index"));
 	for (let link in ilinks) {
 		let l = ilinks[link];
-		$(l["id"]).text(l["txt"] + to_the_top);
+		$(l["id"]).text(`<a href="#ws-index-list">${l["txt"]}${to_the_top}</a>`);
 		$(l["id"]).append(`<a id="${l["id"]}"></a>`);
 		$("#ws-index-list").append(`<li><a href="${l["id"]}">${l["txt"]}</a></li>`);
 	}
