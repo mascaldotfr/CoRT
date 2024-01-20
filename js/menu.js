82713e4956414a2b21bc655fe56b15db827e0b49
mascal <116852972+mascaldotfr@users.noreply.github.com>
2024-01-20 19:01:23 +0100

    WSTATS: add "to the top" links, index is now a card
    Proposed by Vlata, thanks!

--- js/menu.js
+++ js/menu.js
@@ -53,7 +53,7 @@ let __menu_footer = `
 		<p><i>CoRT is a free and open source website, feel free to check out its
 		<a href="https://github.com/mascaldotfr/CoRT" target="_blank">source code</a>, and report
 		<a href="https://github.com/mascaldotfr/CoRT/wiki/Bug-reports" target="_blank">bugs</a>.
-		<!--VERSION-->Version: 20240120.172818
+		<!--VERSION-->Version: 20240120.180123
 		</i></p>
 `;
 
