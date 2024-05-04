feb93a6c5d04a6801c5bd8f2d92e89624c348347
mascal <116852972+mascaldotfr@users.noreply.github.com>
2024-05-04 09:56:34 +0200

    WEVENTS: fix the container size

--- js/menu.js
+++ js/menu.js
@@ -55,7 +55,7 @@ let __menu_footer = `
 		<p><i>CoRT is a free and open source website, feel free to check out its
 		<a href="https://github.com/mascaldotfr/CoRT" target="_blank">source code</a>, and report
 		<a href="https://github.com/mascaldotfr/CoRT/wiki/Bug-reports" target="_blank">bugs</a>.
-		<!--VERSION-->Version: 20240503.182024
+		<!--VERSION-->Version: 20240504.075634
 		</i></p>
 `;
 
