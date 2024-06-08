0ab0e40ed97abf534b7ffc373f628b00c31c3a74
Joshua Treudler <joshua@treudler.net>
2024-06-08 07:11:08 +0200

    modify fortsheld_byrealm chartist options to start at 0

--- js/wstats.js
+++ js/wstats.js
@@ -115,7 +115,7 @@ function show_graphs_fortsheld_byrealm(data, selector) {
 		axisX: { onlyInteger: true },
 		horizontalBars: true,
 		seriesBarDistance: 30,
-		low: Math.min(...series) * 0.95,
+		low: Math.min(...series) * 0,
 		high: Math.max(...series) * 1.05
 	};
 	new Chartist.BarChart(selector, dataset, options);
