3c3caca2ece09b4688ca38e831db8ecf241118d2
mascal <116852972+mascaldotfr@users.noreply.github.com>
2024-07-18 17:36:08 +0200

    TRAINER: Add Needle Blast's area and arc

--- js/menu.js
+++ js/menu.js
@@ -58,7 +58,7 @@ let __menu_footer = `
 		<a href="https://github.com/mascaldotfr/CoRT/wiki/Bug-reports" target="_blank">bugs</a>.
 		See also the <a href="https://discord.gg/P5BJRtTx3R">Discord server</a> and
 		<a href="irc://irc.quakenet.org/regnum-online">IRC channel</a>!</i></p>
-		<p> <!--VERSION-->Version: 20240715.163428
+		<p> <!--VERSION-->Version: 20240718.153608
 `;
 
 $(document).ready(function() {
