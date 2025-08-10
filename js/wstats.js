ba9a7c0dedeb62587a0745a1ab7037f12c195202
mascal <116852972+mascaldotfr@users.noreply.github.com>
2025-08-10 11:00:43 +0200

    GLOBAL: update chartist.js to 1.4.0

--- js/wstats.js
+++ js/wstats.js
@@ -5,7 +5,7 @@ import {get_realm_colors, get_realms} from "./wztools/constants.js";
 import {translate_fort} from "./wztools/translate_forts.js";
 import {timestamp_ago, timestamp_now} from "./wztools/time.js";
 import {$onlinemanager} from "./libs/onlinemanager.js";
-import {__chartist_responsive} from "./libs/chartist-1.3.0.min.js";
+import {__chartist_responsive} from "./libs/chartist.js";
 
 let to_the_top = `&nbsp;<svg style="height:1.5em; margin-bottom:0px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M0 0h512v512H0z" fill-opacity="0"></path><path d="M256 47L139.4 202.467l93.6-40.115V359h46V162.352l93.6 40.115L256 47zM144 256L32 480h448L368 256h-71v121h-82V256h-71z" fill="#9ccdf9" fill-opacity="1"></path></svg>`;
 
