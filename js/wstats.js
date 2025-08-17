f3b3f8fa146e2b1db61a4c406733d56176299f69
mascal <116852972+mascaldotfr@users.noreply.github.com>
2025-08-17 13:30:37 +0200

    WSTATS: make things more compact on mobile

--- js/wstats.js
+++ js/wstats.js
@@ -7,8 +7,6 @@ import {timestamp_ago, timestamp_now} from "./wztools/time.js";
 import {$onlinemanager} from "./libs/onlinemanager.js";
 import {__chartist_responsive} from "./libs/chartist.js";
 
-let to_the_top = `&nbsp;<svg style="height:1.5em; margin-bottom:0px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M0 0h512v512H0z" fill-opacity="0"></path><path d="M256 47L139.4 202.467l93.6-40.115V359h46V162.352l93.6 40.115L256 47zM144 256L32 480h448L368 256h-71v121h-82V256h-71z" fill="#9ccdf9" fill-opacity="1"></path></svg>`;
-
 // sync with statistics.json
 let report_days = [7, 30, 90];
 
@@ -236,7 +234,7 @@ $(document).ready(function() {
 	$("#ws-index-title").text(_("Index"));
 	for (let link in ilinks) {
 		let l = ilinks[link];
-		$(l["id"]).text(`<a href="#ws-index-list">${l["txt"]}${to_the_top}</a>`);
+		$(l["id"]).text(`<a href="#ws-index-list">${l["txt"]}</a>`);
 		$(l["id"]).append(`<a id="${l["id"]}"></a>`);
 		$("#ws-index-list").append(`<li><a href="${l["id"]}">${l["txt"]}</a></li>`);
 	}
