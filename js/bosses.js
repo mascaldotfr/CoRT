eae86b4e551981edd98b848577c13c33dc5e9251
mascal <116852972+mascaldotfr@users.noreply.github.com>
2025-08-17 17:00:42 +0200

    BOSSES/BZ: reduce motion due to addtocalendar use

--- js/bosses.js
+++ js/bosses.js
@@ -50,13 +50,14 @@ function display_next_respawn(boss) {
 	$(`#boss-${boss}-lastspawn`).prepend(`<i>${_("Last respawn")}:
 		${unixstamp2human(previous_respawns[boss])}</i>`);
 	let next_respawn_in = timestamp_ago(next_respawns[boss][0]);
-	$(`#boss-${boss}-respawn`).append(`<li class="red bold">${_("Next respawn in")} ${next_respawn_in.human}</li>`);
+	let next_respawns_html = `<li class="red bold">${_("Next respawn in")} ${next_respawn_in.human}</li>`;
 	for (let respawn in next_respawns[boss]) {
 		let calendar = generate_calendar(next_respawns[boss][respawn], boss, 900);
 		let color = respawn == 0 ? "green" : "faded";
-		$(`#boss-${boss}-respawn`).append(`<li class="${color}"><b>
-			${unixstamp2human(next_respawns[boss][respawn])}</b>${calendar}</li>`);
+		next_respawns_html += `<li class="${color}"><b>
+			${unixstamp2human(next_respawns[boss][respawn])}</b>${calendar}</li>`;
 	}
+	$(`#boss-${boss}-respawn`).append(next_respawns_html);
 	let bossname = boss.charAt(0).toUpperCase() + boss.slice(1);
 	if (next_respawn_in["days"] == 0 && next_respawn_in["hours"] == 0) {
 		if (next_respawn_in["minutes"] <= 10 && next_respawn_in["minutes"] >= 1 &&
