c7a5c944c0ee0ba4b366da0ff101370677cda8cb
mascal <116852972+mascaldotfr@users.noreply.github.com>
2025-08-17 15:41:22 +0200

    Global: more CLS fixes
    Network throttling does wonders for testing!

--- js/menu.js
+++ js/menu.js
@@ -57,7 +57,7 @@ let __menu_footer = `
 		<a href="https://github.com/mascaldotfr/CoRT" target="_blank">source code</a>, and report
 		<a href="https://github.com/mascaldotfr/CoRT/wiki/Bug-reports" target="_blank">bugs</a>.
 		See also the <a href="https://discord.gg/P5BJRtTx3R">Discord server</a>!</i></p>
-		<p> <!--VERSION-->Version: 20250817.122144
+		<p> <!--VERSION-->Version: 20250817.134122
 `;
 
 $(document).ready(function() {
