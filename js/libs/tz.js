3047215754ff723febfb62152a824a5435a77a27
mascal <116852972+mascaldotfr@users.noreply.github.com>
2025-08-20 19:40:04 +0200

    tz picker: cache the generated html in localstorage
    This is expensive according to profiling, and caching reduces by 80%
    the <select> generation. Put a 7days cache, which is good enough for
    somethign that very rarely changes.

--- js/libs/tz.js
+++ js/libs/tz.js
@@ -17,9 +17,35 @@ function get_system_tz() {
 	return tz;
 }
 
+function display_tz_list(selector, options, storedtz) {
+	let target = document.querySelector(selector);
+	target.innerHTML = options;
+	target.addEventListener("change", function (x) {
+		localStorage.setItem("tz", target.value);
+		location.reload();
+	});
+	target.value = storedtz;
+}
+
 export function create_tz_list(selector) {
+
 	let human_tzs = {};
+	let now = Date.now();
+	let cache_duration = 1000 * 3600 * 24 * 7; // 7-day cache
 	let storedtz = localStorage.getItem("tz");
+	let cached_tz_list = localStorage.getItem("tzlisthtml");
+	let cached_tz_list_ts = localStorage.getItem("tzlisthtmlts");
+
+	// Cache HIT
+	if (cached_tz_list != null && cached_tz_list_ts != null &&
+	    now - cached_tz_list_ts <= cache_duration) {
+		display_tz_list(selector, cached_tz_list, storedtz);
+		return;
+	}
+
+	// Cache MISS
+	console.info("create_tz_list: cache timeout");
+	localStorage.setItem("tzlisthtmlts", now);
 
 	try {
 		for (let tz of Intl.supportedValuesOf("timeZone")) {
@@ -39,15 +65,12 @@ export function create_tz_list(selector) {
 	let options = "";
 	for (let tz of Object.keys(human_tzs).sort())
 		options += `<option value="${human_tzs[tz]}">${tz}</option>`;
-	let target = document.querySelector(selector);
-	target.innerHTML = options;
-	target.addEventListener("change", function (x) {
-		localStorage.setItem("tz", target.value);
-		location.reload();
-	});
 
 	if (storedtz === undefined || Object.values(human_tzs).indexOf(storedtz) < 0)
 		storedtz = get_system_tz();
 	localStorage.setItem("tz", storedtz);
-	target.value = storedtz;
+
+	display_tz_list(selector, options, storedtz);
+
+	localStorage.setItem("tzlisthtml", options)
 }
