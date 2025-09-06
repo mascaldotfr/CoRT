2a8db63850c81aa38d61fe47d26e1a28d490a301
mascal <116852972+mascaldotfr@users.noreply.github.com>
2025-09-06 18:09:41 +0200

    WZ/WEVENTS: optimize events display
    Profiling showed using Intl.DateTimeFormat instead of
    Date.toLocaleDateString is 7 times faster. I may consider using it in
    other places.
    
    Also, don't run notify code if notifications aren't enabled.

--- js/wztools/wztools.js
+++ js/wztools/wztools.js
@@ -12,79 +12,88 @@ export class HumaniseEvents {
 	// (unix timestamp from the last event) for use in a desktop notification
 
 	humanise_events(events, has_id=true, notify=0)  {
+		// datetime
+		let tz = localStorage.getItem("tz");
+		const dformatter = new Intl.DateTimeFormat(undefined, {
+			month: 'numeric', day: 'numeric',
+			hour: '2-digit', minute: '2-digit', timeZone: tz
+		});
+
+		// wztools
 		let translator = new TranslateForts();
 		let constants = new Constants();
-		let tz = localStorage.getItem("tz");
-		let date_options = {
-			month: 'numeric', day: 'numeric',
-			hour: '2-digit', minute: '2-digit', timeZone: tz };
-		let events_html = "";
-		let events_notify = "";
 		let realm_colors = constants.realm_colors;
+
+		let events_html = new Array();
+		let events_notify = new Array();
+
 		for (let anevent of events) {
 			let dt = new Date(anevent["date"] * 1000);
-			let datetime = dt.toLocaleDateString(undefined, date_options);
-			let owner = anevent["owner"];
-			let owner_color = realm_colors[owner];
+			let datetime = dformatter.format(dt);
+			let owner_color = realm_colors[anevent["owner"]];
 			let location_color = realm_colors[anevent["location"]];
 			let captured = anevent["name"];
 			let captured_notify = anevent["name"];
 			let dt_color = anevent["type"] == "wish" ? location_color: "";
-			events_html += `<span class="${dt_color} bold">${datetime}</span>&nbsp;`;
+			events_html.push(`<span class="${dt_color} bold">${datetime}</span>&nbsp;`);
 			if (anevent["type"] == "fort" || anevent["type"] == "gem") {
 				let location_color = realm_colors[anevent["location"]];
 				if (anevent["type"] == "fort") {
 					captured = translator.translate_fort(captured, has_id);
 					if (has_id === true) {
 						captured = captured.substring(0, captured.lastIndexOf(" "));
-						captured_notify = captured;
+						if (notify > 0)
+							captured_notify = captured;
 					}
 				}
 				else if (anevent["type"] == "gem") {
 					captured = `${_("Gem")} #${captured}`;
-					captured_notify = `${captured} [${anevent["location"]}]`;
+					if (notify > 0)
+						captured_notify = `${captured} [${anevent["location"]}]`;
 				}
 				let target = `<span class="${location_color} bold">${captured}</span>`;
 				let action;
 				let action_notify;
 				if (anevent["location"] == anevent["owner"]) {
 					action = _("has recovered %s", target);
-					action_notify = _("has recovered %s", captured_notify);
+					if (notify > 0)
+						action_notify = _("has recovered %s", captured_notify);
 				}
 				else {
 					action = _("has captured %s", target);
-					action_notify = _("has captured %s", captured_notify);
+					if (notify > 0)
+						action_notify = _("has captured %s", captured_notify);
 				}
-				events_html += `<span class="${owner_color} bold">${owner}</span> ${action}.`;
+				events_html.push(`<span class="${owner_color} bold">${anevent["owner"]}</span> ${action}.`);
 				if (notify > 0 && anevent["date"] >= notify)
-					events_notify += `${owner} ${action_notify}\n`;
+					events_notify.push(`${anevent["owner"]} ${action_notify}`);
 			}
 			else if (anevent["type"] == "relic") {
 				let location_color = realm_colors[anevent["owner"]];
 				let relic = `<span class="${location_color} bold">${_("%s's relic", captured)}</span>`;
 				let relic_notify = `${_("%s's relic", captured)}`;
 				if (anevent["location"] == "altar") {
-					events_html += `${relic} ${_("is back.")}`;
+					events_html.push(`${relic} ${_("is back.")}`);
 					if (notify > 0 && anevent["date"] >= notify)
-						events_notify += `${relic_notify} ${_("is back.")}\n`;
+						events_notify.push(`${relic_notify} ${_("is back.")}`);
 				}
 				else {
-					events_html += `${relic} ${_("is in transit.")}`;
+					events_html.push(`${relic} ${_("is in transit.")}`);
 					if (notify > 0 && anevent["date"] >= notify)
-						events_notify += `${relic_notify} ${_("is in transit.")}\n`;
+						events_notify.push(`${relic_notify} ${_("is in transit.")}`);
 				}
 			}
 			else if (anevent["type"] == "wish") {
 				let sentence = _("%s made a dragon wish!", anevent["location"]);
-				events_html += `<span class="${location_color} bold">${sentence}</span>`;
+				events_html.push(`<span class="${location_color} bold">${sentence}</span>`);
 				if (notify > 0 && anevent["date"] >= notify)
-					events_notify += sentence + "\n";
+					events_notify.push(sentence);
 			}
-			events_html += `<br>`;
+			events_html.push("<br>");
 		}
 		if (notify > 0)
-			return [events_html, events_notify];
-		return events_html;
+			return [events_html.join(""), events_notify.join("\n")];
+		return events_html.join("");
 	}
 }
 
