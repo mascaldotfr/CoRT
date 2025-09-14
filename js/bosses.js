mascaldotfr/CoRT

commit 48f7681bc8c2c733f55abc8c54d5063355346e9f

GLOBAL: sprinkle Intl.DateTimeFormat where it counts
And various other small fixes

js/bosses.js
@@ -5,6 +5,11 @@ import {Time} from "./wztools/wztools.js";
 // wztools
 let time = new Time();
 
+let dformatter = new Intl.DateTimeFormat(localStorage.getItem("lang"), {
+		hour12: false, weekday: 'long', month: 'long', day: 'numeric',
+		hour: 'numeric', minute: 'numeric', timeZone: localStorage.getItem("tz")
+});
+
 // The last respawn timestamp in UTC time
 // You can update it by looking at your browser console and getting the last
 // respawn timestamps. At least yearly, since the get_next_respawns() loop
@@ -19,12 +24,7 @@ let previous_respawns = first_respawns;
 let notified_10m = false;
 
 function unixstamp2human(unixstamp) {
-	let dt = new Date(unixstamp * 1000);
-	let lang = localStorage.getItem("lang");
-	let tz = localStorage.getItem("tz");
-	return dt.toLocaleDateString(lang, {
-		hour12: false, weekday: 'long', month: 'long', day: 'numeric',
-		hour: 'numeric', minute: 'numeric', timeZone: tz});
+	return dformatter.format(new Date(unixstamp * 1000));
 }
 
 function get_next_respawns(boss) {

js/bz.js
@@ -1,6 +1,17 @@
 import {$, insert_notification_link, mynotify, generate_calendar} from "./libs/cortlibs.js";
 import {_} from "../data/i18n.js";
 
+let tz = localStorage.getItem("tz");
+let lang = localStorage.getItem("lang");
+let dformatter = new Intl.DateTimeFormat(lang, {
+	weekday: 'long', month: '2-digit', day: 'numeric',
+	hour: '2-digit', minute: '2-digit', timeZone: tz
+});
+let tformatter = new Intl.DateTimeFormat(lang, {
+	hour: '2-digit', minute: '2-digit', timeZone: tz
+});
+
+
 // XXX ALL TIMES ARE UTC INTERNALLY
 // SUNDAY = 0 SATURDAY = 6
 let bz_begin = [ 	[13, 18],
@@ -52,12 +63,6 @@ function feed_bz(init=false) {
 	let next_bzs_end = [];
 	let bz_on = false;
 	let bz_ends_at = 0;
-	let tz = localStorage.getItem("tz");
-	let date_options = {
-		weekday: 'long', month: '2-digit', day: 'numeric',
-		hour: '2-digit', minute: '2-digit', timeZone: tz};
-	let time_options = {hour: '2-digit', minute: '2-digit', timeZone: tz};
-	let lang = localStorage.getItem("lang");
 
 	let current_date = new Date();
 	let current_day = parseInt(current_date.getUTCDay());
@@ -123,8 +128,8 @@ function feed_bz(init=false) {
 	for (let next_bz in next_bzs_begin) {
 		let bz_begin_date = new Date(next_bzs_begin[next_bz]);
 		let bz_end_date = new Date(next_bzs_end[next_bz]);
-		let bz_begin_datetime = bz_begin_date.toLocaleDateString(lang, date_options);
-		let bz_end_time = bz_end_date.toLocaleTimeString(lang, time_options);
+		let bz_begin_datetime = dformatter.format(bz_begin_date);
+		let bz_end_time = tformatter.format(bz_end_date);
 		let duration = (bz_end_date.getTime() - bz_begin_date.getTime()) / 1000;
 		let calendar = generate_calendar(bz_begin_date.getTime() / 1000, "Battlezone", duration);
 		bz_next_future += `<li>${bz_begin_datetime} - ${bz_end_time}

js/menu.js
@@ -56,7 +56,7 @@ let __menu_footer = `
 		<a href="https://github.com/mascaldotfr/CoRT" target="_blank">source code</a>, and report
 		<a href="https://github.com/mascaldotfr/CoRT/wiki/Bug-reports" target="_blank">bugs</a>.
 		See also the <a href="https://discord.gg/P5BJRtTx3R">Discord server</a>!</i></p>
-		<p> <!--VERSION-->Version: 20250906.165755
+		<p> <!--VERSION-->Version: 20250906.174708
 `;
 
 $(document).ready(function() {

js/wstats.js
@@ -4,6 +4,8 @@ import {_} from "../data/i18n.js";
 import {Constants, TranslateForts, Time} from "./wztools/wztools.js";
 import {__chartist_responsive} from "./libs/chartist.js";
 
+
+
 // sync with statistics.json
 let report_days = [7, 30, 90];
 
@@ -12,9 +14,12 @@ let constants = new Constants();
 let xlator = new TranslateForts();
 let time = new Time();
 
-let tz = localStorage.getItem("tz");
 let realm_colors = constants.realm_colors;
 let realms = constants.realm_names;
+
+let tformatter = new Intl.DateTimeFormat(localStorage.getItem("lang"), {
+	hour12: false, hour: 'numeric', minute: 'numeric', timeZone: localStorage.getItem("tz")
+});
 
 // undefined / null => N/A or 0
 function naify(value, failover="0") {
@@ -32,8 +37,7 @@ function localize_timelines(utclines) {
 		let localline = [];
 		for (let hour in utclines[realm]) {
 			date.setUTCHours(hour, 0, 0, 0);
-			let date_options = {hour12: false, hour: 'numeric', minute: 'numeric', timeZone: tz};
-			let localhour = date.toLocaleTimeString(undefined, date_options);
+			let localhour = tformatter.format(date);
 			localhour = localhour.replace(/:\d+.+$/, "");
 			localhour = localhour.replace(/^0/, "");
 			localline[localhour] = utclines[realm][hour];

js/wz.js
@@ -198,7 +198,6 @@ async function display_wz(init=false) {
 
 	// Last update
 
-	wz_lastupdate = Math.floor(new Date().getTime() / 1000);
 	if (events_list[1].length > 0)
 		mynotify(_("WZ status"), events_list[1], "wz");
 }

js/wztools/wztools.js
@@ -14,7 +14,7 @@ export class HumaniseEvents {
 	humanise_events(events, has_id=true, notify=0)  {
 		// datetime
 		let tz = localStorage.getItem("tz");
-		const dformatter = new Intl.DateTimeFormat(undefined, {
+		let dformatter = new Intl.DateTimeFormat(undefined, {
 			month: 'numeric', day: 'numeric',
 			hour: '2-digit', minute: '2-digit', timeZone: tz
 		});

commit 4713b151191b8132abc55f079e93b5b81c063c0a

BZ/BOSSES: fix a race condition if visited for the frist time
It appears that if you visit the page for the first time, language or tz
in localStorage is null when date and time formatters are created,
leading to a blank page due to a race condition.

Move all these thing in document.ready().

The bug has been introduced by 48f7681 a week ago, and some peeps
probably met it without reporting. Sad.

Survived a few private browsing sessions, that I should have used
earlier. Maybe I should assert tz and lang, maybe ...to be continued.

js/bosses.js
@@ -5,10 +5,8 @@ import {Time} from "./wztools/wztools.js";
 // wztools
 let time = new Time();
 
-let dformatter = new Intl.DateTimeFormat(localStorage.getItem("lang"), {
-		hour12: false, weekday: 'long', month: 'long', day: 'numeric',
-		hour: 'numeric', minute: 'numeric', timeZone: localStorage.getItem("tz")
-});
+// date formatter
+let dformatter = null;
 
 // The last respawn timestamp in UTC time
 // You can update it by looking at your browser console and getting the last
@@ -101,6 +99,10 @@ $(document).ready(function() {
 	document.title = "CoRT - " + _("Bosses respawn countdown");
 	$("#title").text(_("Bosses respawn countdown"));
 	$("#boss-info").text(_("The page refreshes itself every minute."));
+	dformatter = new Intl.DateTimeFormat(localStorage.getItem("lang"), {
+		hour12: false, weekday: 'long', month: 'long', day: 'numeric',
+		hour: 'numeric', minute: 'numeric', timeZone: localStorage.getItem("tz")
+	});
 
 	insert_notification_link();
 	refresh_display();

js/bz.js
@@ -1,16 +1,9 @@
 import {$, insert_notification_link, mynotify, generate_calendar} from "./libs/cortlibs.js";
 import {_} from "../data/i18n.js";
 
-let tz = localStorage.getItem("tz");
-let lang = localStorage.getItem("lang");
-let dformatter = new Intl.DateTimeFormat(lang, {
-	weekday: 'long', month: '2-digit', day: 'numeric',
-	hour: '2-digit', minute: '2-digit', timeZone: tz
-});
-let tformatter = new Intl.DateTimeFormat(lang, {
-	hour: '2-digit', minute: '2-digit', timeZone: tz
-});
-
+// time and date formatters
+let dformatter = null;
+let tformatter = null;
 
 // XXX ALL TIMES ARE UTC INTERNALLY
 // SUNDAY = 0 SATURDAY = 6
@@ -144,6 +137,17 @@ $(document).ready(function() {
 	document.title = "CoRT - " + _("BZ status");
 	$("#title").text(_("BZ status"));
 	$("#bz-next-title").text(_("Next BZ:"));
+
+	let tz = localStorage.getItem("tz");
+	let lang = localStorage.getItem("lang");
+	dformatter = new Intl.DateTimeFormat(lang, {
+		weekday: 'long', month: '2-digit', day: 'numeric',
+		hour: '2-digit', minute: '2-digit', timeZone: tz
+	});
+	tformatter = new Intl.DateTimeFormat(lang, {
+		hour: '2-digit', minute: '2-digit', timeZone: tz
+	});
+
 	insert_notification_link();
 	feed_bz(true);
 });

js/menu.js
@@ -55,7 +55,7 @@ let __menu_footer = `
 		<a href="https://github.com/mascaldotfr/CoRT/wiki/Bug-reports" target="_blank">bugs</a>.
 		See also the <a href="https://discord.gg/P5BJRtTx3R">Discord server</a>,
 		and the <a href="https://hail.thebus.top/CoRT/">faster website</a>!</i></p>
-		<p> <!--VERSION-->Version: 20250914.111201
+		<p> <!--VERSION-->Version: 20250914.190753
 `;
 
 $(document).ready(function() {