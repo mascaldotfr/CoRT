c7ab1bb35be639777bfe8a8d597536a09c3327ae
mascal <116852972+mascaldotfr@users.noreply.github.com>
2025-09-27 10:59:01 +0200

    DEPLOY: naivety fixes for the Caddyfile

--- js/menu.js
+++ js/menu.js
@@ -55,7 +55,7 @@ let __menu_footer = `
 		<a href="https://github.com/mascaldotfr/CoRT/wiki/Bug-reports" target="_blank">bugs</a>.
 		See also the <a href="https://discord.gg/P5BJRtTx3R">Discord server</a>,
 		and the <a href="https://hail.thebus.top/CoRT/">faster website</a>!</i></p>
-		<p> <!--VERSION-->Version: 20250925.204336
+		<p> <!--VERSION-->Version: 20250927.085901
 `;
 
 $(document).ready(function() {
