4f124de77f6b1657a059f8ff9f1f46396e5c9394
mascal <116852972+mascaldotfr@users.noreply.github.com>
2025-10-15 21:56:36 +0200

    APIs: let localhost run the official API

--- js/api_url.js
+++ js/api_url.js
@@ -11,7 +11,9 @@ import {_} from "../data/i18n.js";
 
 function get_paths() {
 	// https://mascaldotfr.github.io/CoRT uses an external API server as an exception
-	if (window.location.hostname === "mascaldotfr.github.io") {
+	// Also allow local testing to use the official API
+	let frontend_with_no_api = ["localhost", "mascaldotfr.github.io"];
+	if (frontend_with_no_api.includes(window.location.hostname)) {
 		let api = {};
 		// The root where all API files can be found
 		api["base"] = "https://cort.thebus.top/api";
