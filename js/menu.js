6e30d6151c8f9cca261b481fb14f80f0a926e2da
mascal <116852972+mascaldotfr@users.noreply.github.com>
2025-10-17 19:27:36 +0200

    GLOBAL: hide github links out of mascaldotfr.github.io
    This allows to customize stuff for 3rd party.

--- js/menu.js
+++ js/menu.js
@@ -48,14 +48,17 @@ let __menu_content = `
 		</ul>
 	</header>
 `;
+
+let __menu_github_stuff = window.location.host !== "mascaldotfr.github.io" ? '' : `
+	<i>CoRT is a free and open source website, feel free to check out its
+	<a href="https://github.com/mascaldotfr/CoRT" target="_blank">source code</a>, and report
+	<a href="https://github.com/mascaldotfr/CoRT/wiki/Bug-reports" target="_blank">bugs</a>.
+`;
 let __menu_footer = `
-		<div id="tz"><div id="tztitle">${_("Timezone:")}&nbsp;</div><select id="tzchooser"></select></div>
-		<p><i>CoRT is a free and open source website, feel free to check out its
-		<a href="https://github.com/mascaldotfr/CoRT" target="_blank">source code</a>, and report
-		<a href="https://github.com/mascaldotfr/CoRT/wiki/Bug-reports" target="_blank">bugs</a>.
-		See also the <a href="https://discord.gg/P5BJRtTx3R">Discord server</a>,
-		and the <a href="https://cort.thebus.top">faster website</a>!</i></p>
-		<p> <!--VERSION-->Version: 20251016.215833
+	<div id="tz"><div id="tztitle">${_("Timezone:")}&nbsp;</div><select id="tzchooser"></select></div>
+	<p>${__menu_github_stuff}
+	See also the <a href="https://discord.gg/P5BJRtTx3R">Discord server</a>!</p>
+	<p> <!--VERSION-->Version: 20251017.172736
 `;
 
 $(document).ready(function() {
