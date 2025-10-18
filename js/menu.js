cb538a630997643cdcc41fd676c5639a1c77eb7a
mascal <116852972+mascaldotfr@users.noreply.github.com>
2025-10-18 10:48:28 +0200

    GLOBAL: improve the release GH Action

--- js/menu.js
+++ js/menu.js
@@ -58,7 +58,7 @@ let __menu_footer = `
 	<div id="tz"><div id="tztitle">${_("Timezone:")}&nbsp;</div><select id="tzchooser"></select></div>
 	<p>${__menu_github_stuff}
 	See also the <a href="https://discord.gg/P5BJRtTx3R">Discord server</a>!</p>
-	<p> <!--VERSION-->Version: 20251017.215711
+	<p> <!--VERSION-->Version: 20251018.084828
 `;
 
 $(document).ready(function() {
