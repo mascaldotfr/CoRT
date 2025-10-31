3ccb4f1ca1b58a2b4f061fb59e2c6446d3c4b8d6
mascal <116852972+mascaldotfr@users.noreply.github.com>
2025-10-31 18:46:39 +0100

    GLOBAL: call maintenance API only every 5 minutes
    It's a complete overkill to do this at each hit

--- js/menu.js
+++ js/menu.js
@@ -57,7 +57,7 @@ let __menu_footer = `
 	<div id="tz"><div id="tztitle">${_("Timezone:")}&nbsp;</div><select id="tzchooser"></select></div>
 	<p>${__menu_github_stuff}
 	See also the <a href="https://discord.gg/P5BJRtTx3R">Discord server</a>!</p>
-	<p> <!--VERSION-->Version: 20251030.210118
+	<p> <!--VERSION-->Version: 20251031.174639
 `;
 
 $(document).ready(function() {
@@ -100,8 +100,17 @@ $(document).ready(function() {
 
 	// Put maintenance message (see /api/MAINTENANCE.md)
 	setTimeout(async () => {
+		const ts = Date.now();
+		const last_check = parseInt(localStorage.getItem("maint_last_check")) || 0;
+		let msg = localStorage.getItem("maint_msg") || "";
 		try {
-			const msg = await $().get(__api__urls["maintenance"]);
+			// need to refresh the cache, either at start or if >=
+			// 5 minutes after last fetch
+			if (last_check == 0 ||  ts >= last_check + 5 * 60 * 1000) {
+				msg = await $().get(__api__urls["maintenance"]);
+				localStorage.setItem("maint_msg", msg);
+				localStorage.setItem("maint_last_check", ts);
+			}
 			// Bail out on empty message (404s fall here as well)
 			if (msg.length == 0)
 				return;
