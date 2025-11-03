329d4db9bcccccc97d831f912598feff7a2dc1cd
mascal <116852972+mascaldotfr@users.noreply.github.com>
2025-11-03 11:16:11 +0000

    TSTATS: fix bad index height on small devices (512-800px)

--- js/menu.js
+++ js/menu.js
@@ -57,7 +57,7 @@ let __menu_footer = `
 	<div id="tz"><div id="tztitle">${_("Timezone:")}&nbsp;</div><select id="tzchooser"></select></div>
 	<p>${__menu_github_stuff}
 	See also the <a href="https://discord.gg/P5BJRtTx3R">Discord server</a>!</p>
-	<p> <!--VERSION-->Version: 20251103.095211
+	<p> <!--VERSION-->Version: 20251103.111611
 `;
 
 $(document).ready(function() {
