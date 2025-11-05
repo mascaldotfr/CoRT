ddd14722d84f1b1f28d9dca3489bcbd49404e876
mascal <116852972+mascaldotfr@users.noreply.github.com>
2025-11-05 09:10:22 +0000

    MENU: put all languages inline on mobile

--- js/menu.js
+++ js/menu.js
@@ -61,7 +61,7 @@ let __menu_footer = `
 	<div id="tz"><div id="tztitle">${_("Timezone:")}&nbsp;</div><select id="tzchooser"></select></div>
 	<p>${__menu_github_stuff}
 	See also the <a href="https://discord.gg/P5BJRtTx3R">Discord server</a>!</p>
-	<p> <!--VERSION-->Version: 20251104.231241
+	<p> <!--VERSION-->Version: 20251105.102323
 `;
 
 $(document).ready(function() {
