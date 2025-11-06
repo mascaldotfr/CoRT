ec1263afcc2e6829c3d22cc4665f210843b03daa
mascal <116852972+mascaldotfr@users.noreply.github.com>
2025-11-06 12:53:28 +0000

    GH actions: remove useless newline when generating cache busting parameter
    Spotted while releasing new sentinel.

--- js/menu.js
+++ js/menu.js
@@ -63,7 +63,7 @@ let __menu_footer = `
 	<div id="tz"><div id="tztitle">${_("Timezone:")}&nbsp;</div><select id="tzchooser"></select></div>
 	<p>${__menu_github_stuff}
 	See also the <a href="https://discord.gg/P5BJRtTx3R">Discord server</a>!</p>
-	<p> <!--VERSION-->Version: 20251105.175244
+	<p> <!--VERSION-->Version: 20251106.125328
 `;
 
 $(document).ready(function() {
