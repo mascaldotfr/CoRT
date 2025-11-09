3f41d5d2505b7575c90b589771858f80e8f3a244
mascal <116852972+mascaldotfr@users.noreply.github.com>
2025-11-09 01:46:09 +0100

    MENU: move misplaced flex element, grow button to take all the space

--- js/menu.js
+++ js/menu.js
@@ -63,7 +63,7 @@ let __menu_footer = `
 	<div id="tz"><div id="tztitle">${_("Timezone:")}&nbsp;</div><select id="tzchooser"></select></div>
 	<p>${__menu_github_stuff}
 	See also the <a href="https://discord.gg/P5BJRtTx3R">Discord server</a>!</p>
-	<p> <!--VERSION-->Version: 20251108.212822
+	<p> <!--VERSION-->Version: 20251109.004609
 `;
 
 $(document).ready(function() {
