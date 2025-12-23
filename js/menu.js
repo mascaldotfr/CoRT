a89704fd9edad5de66267c8bb4c9895dc7464ce6
mascal <116852972+mascaldotfr@users.noreply.github.com>
2025-12-23 09:56:04 +0100

    GLOBAL: add "epicos" as keyword to improve SEO

--- js/menu.js
+++ js/menu.js
@@ -63,7 +63,7 @@ let __menu_footer = `
 	<div id="tz"><div id="tztitle">${_("Timezone:")}&nbsp;</div><select id="tzchooser"></select></div>
 	<p>${__menu_github_stuff}
 	See also the <a href="https://discord.gg/P5BJRtTx3R">Discord server</a>!</p>
-	<p> <!--VERSION-->Version: 20251223.081231
+	<p> <!--VERSION-->Version: 20251223.085604
 `;
 
 $(document).ready(function() {
