76f4f339e3ae2058d6835d8bab567cc2e536e947
mascal <116852972+mascaldotfr@users.noreply.github.com>
2026-03-29 13:02:40 +0200

    i18n: fix a spanish error
    Forgot to change from e to y when I changed the translation of "report"
    to "reportar".

--- js/libs/cortlibs.js
+++ js/libs/cortlibs.js
@@ -521,7 +521,7 @@ export const __i18n__ = {
 	},
 	"CoRT is a free and open source website, feel free to check out its %s, and %s. See also the %s!": {
 		"fr": "CoRT est un site web libre et open source, n'hésitez pas à consulter son %s, et %s. Voyez aussi le %s !",
-		"es": "CoRT es un sitio web libre y de código abierto, no dudes en consultar su %s, e %s. ¡Mira también el %s!",
+		"es": "CoRT es un sitio web libre y de código abierto, no dudes en consultar su %s, y %s. ¡Mira también el %s!",
 		"de": "CoRT ist eine freie und quelloffene Website, schau dir gerne ihren %s und %s an. Schau auch auf den %s!"
 	},
 	"source code": {
