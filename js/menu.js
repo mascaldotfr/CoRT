27498183231221e0a0eb3647a9f6e3f28f685d03
mascal <116852972+mascaldotfr@users.noreply.github.com>
2026-04-19 15:23:39 +0200

    GLOBAL: definitive versions of color scheme
    Machine beat the men on that one. Ignis one in particular was a pain in
    the ass, and because it's red, it quickly turns anti pattern-ish.
    
    Also, make BOSSES/BZ ON color more noticeable.

--- js/menu.js
+++ js/menu.js
@@ -86,7 +86,7 @@ const __menu_footer = function() { return `
 		</div>
 	</div>
 	<p class="italic">${__menu_github_stuff()}
-	<p> <!--VERSION-->Version: 20260419.121112
+	<p> <!--VERSION-->Version: 20260419.132339
 	(<a href="#" id="reset_powers" title="Clear all CoRT cached data. Use this in case of errors.">/reset_powers</a>)
 `; };
 
