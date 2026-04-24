3607a27552d481d7dff8ccd666ac350535c8dfcb
mascal <116852972+mascaldotfr@users.noreply.github.com>
2026-04-24 19:15:07 +0200

    BZ: update BZ times
    Spotted by Vlata, thanks!

--- js/menu.js
+++ js/menu.js
@@ -86,7 +86,7 @@ const __menu_footer = function() { return `
 		</div>
 	</div>
 	<p class="italic">${__menu_github_stuff()}
-	<p> <!--VERSION-->Version: 20260421.200727
+	<p> <!--VERSION-->Version: 20260424.171507
 	(<a href="#" id="reset_powers" title="Clear all CoRT cached data. Use this in case of errors.">/reset_powers</a>)
 `; };
 
