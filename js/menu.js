5c3f7c29c66144dfb5f50d9f5c9717dce317a353
mascal <116852972+mascaldotfr@users.noreply.github.com>
2026-08-22 00:21:30 +0200

    Tentative fix for previous in older browsers

--- js/menu.js
+++ js/menu.js
@@ -62,7 +62,7 @@ const __menu_footer = function() { return `
 		</div>
 	</div>
 	<p class="italic">${__menu_github_stuff()}
-	<p> <!--VERSION-->Version: 20260821.215141
+	<p> <!--VERSION-->Version: 20260821.222130
 	(<a href="#" id="reset_powers" title="Clear all CoRT cached data. Use this in case of errors.">/reset_powers</a>)
 `; };
 
