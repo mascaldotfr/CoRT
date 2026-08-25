d43016c163caf109ff0047c060362f6f43d90810
mascal <116852972+mascaldotfr@users.noreply.github.com>
2026-08-25 18:31:45 +0200

    GLOBAL: preload cortlibs
    Log analysis is showing not having cortlibs on top reduces performance.

--- js/menu.js
+++ js/menu.js
@@ -62,7 +62,7 @@ const __menu_footer = function() { return `
 		</div>
 	</div>
 	<p class="italic">${__menu_github_stuff()}
-	<p> <!--VERSION-->Version: 20260824.162442
+	<p> <!--VERSION-->Version: 20260825.163145
 	(<a href="#" id="reset_powers" title="Clear all CoRT cached data. Use this in case of errors.">/reset_powers</a>)
 `; };
 
