1effc8261977c3432fb02e97c03e050ccd1fb39f
mascal <116852972+mascaldotfr@users.noreply.github.com>
2026-08-25 22:07:46 +0200

    GLOBAL: reduce FOUC by setting quickly a default background color

--- js/menu.js
+++ js/menu.js
@@ -62,7 +62,7 @@ const __menu_footer = function() { return `
 		</div>
 	</div>
 	<p class="italic">${__menu_github_stuff()}
-	<p> <!--VERSION-->Version: 20260825.191520
+	<p> <!--VERSION-->Version: 20260825.200746
 	(<a href="#" id="reset_powers" title="Clear all CoRT cached data. Use this in case of errors.">/reset_powers</a>)
 `; };
 
