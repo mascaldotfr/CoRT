d872a53a35c2439947050cc0cdfc850573d821e8
mascal <116852972+mascaldotfr@users.noreply.github.com>
2026-09-15 18:56:21 +0200

    libs/uitools: add is_inactive()
    This detect when the page is hidden (switched to another tab or
    minimized) and notifications are off.
    
    The idea is it's useless to fetch something the browser don't need atm;
    MyScheduler will fire an update when the page is visible again.

--- js/libs/uitools.js
+++ js/libs/uitools.js
@@ -8,8 +8,10 @@ export class UITools {
 	static defer() {
 		import("../defer.js");
 	}
+	static is_inactive() {
+		// The goal here is to detect when to do nothing (hidden window
+		// and notifications off). MyScheduler will rehydrate pages
+		// when needed.
+		return (document.hidden && 'Notification' in window && Notification.permission !== 'granted');
+	}
 }
-
-
-
-
